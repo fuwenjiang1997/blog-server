52449db8de119d00dbd699d6f86ad9cf4e0ba126
fuwenjiang <Fwj111>
2020-06-01 13:24:48 +0800

    set router

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +11,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res, next) => {
+    res.header({
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': req.headers.origin || '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'PUT,GET,POST,DELETE,OPTIONS',
+        'Content-Type': 'application/json;charset=utf-8'
+    })
+    if(req.method === 'OPTIONS') {
+        res.sendStatus(200)
+    } else {
+        next()
+    }
+})
+
+app.use('/', require('./routes/index'));
 
 module.exports = app;
